bf780788e0967f69ff1ee2bbf9c4d05b6ac769cd
drakeaifoce <drakeaifoce@gmail.com>
2022-06-12 19:18:08 +0600

    feat: init client

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+        <h1 className="p-10 text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
